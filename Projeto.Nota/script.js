12fe231740e0a8a92ab83f2382649b4b4f93d68a
2507 <mreginadasilva@yahoo.com.br>
2019-04-22 11:55:15 -0300

    projetonotaok

---
+++ Projeto.Nota/script.js
@@ -0,0 +1,17 @@
+function media(){
+    let nota1 = parseFloat(document.getElementById("nota1").value);
+    let nota2 = parseFloat(document.getElementById("nota2").value); 
+    let media = (nota1 + nota2 )/2;
+
+if (media>=7) 
+if (media == 10)
+alert("aprovado com distinção🎉") 
+// se for exatamente igual a 10
+
+else 
+alert ("Aprovado🎉"  +  media )
+
+else 
+    alert("reprovado!😔")
+   
+}
