3597468e1c436183c62e496740b93d60a3f5a0de
mi2507 <mreginadasilva@yahoo.com.br>
2019-04-23 16:27:44 -0300

    exemplodewhile

---
+++ exemplo.while/script.js
@@ -0,0 +1,34 @@
+// senha 1234 
+
+// faça 
+// usuario , qual tua senha 
+// - enquanto errar 
+
+// se usuario acerta 
+// - acerto 
+
+// senao 
+// - ele volta para a pergunta 
+
+
+
+// let senha ;
+// senha = prompt ("digite sua senha");
+
+// function entrar(){
+    let senha ;
+do {
+    senha = prompt("Digite sua senha:");
+    if (senha == '1234')
+        alert ( "Bem vindo ao sistema !😉");
+
+    else 
+    alert("Senha incorreta, digite novamente.🤔");
+
+} while (senha !== '1234');
+// }
+
+
+
+
+
